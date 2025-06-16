6d57462550290e9ad4ac366669cbfa16c837798e
amzyD <lukasz.dyzma1999@gmail.com>
2025-06-16 23:09:22 +0200

    work in progress on count_chards in stats

--- main.py
+++ main.py
@@ -3,21 +3,18 @@ Bookboot
 current functionality: none
 only thing that exists here is a function that returns content of the book
 '''
-from stats import count_words
+from stats import count_words, count_chars
 
 def get_book_text(book_path):
     with open(book_path) as f:
         book_content = f.read()
     return book_content
 
-
-def count_words(text):
-    words = text.split()
-    return len(words)
-
 def main():
     book_txt = get_book_text("./books/frankenstein.txt")
     word_count = count_words(book_txt)
     print(f"{word_count} words found in the document")
+    char_count = count_chars(book_txt)
+    #print(f"{char_count} characters found in the document")
     
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,10 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_chars(text):
+
+    text_lowercase = text.lower()
+    words = text.split()
+    print(words)
+    return words
